1a0bd50e9b054167b75da248d07ba463d1af93f0
xingduo <1451953028@qq.com>
2018-10-25 23:31:24 +0800

    更新

--- util/findImage.py
+++ util/findImage.py
@@ -7,6 +7,7 @@ import os
 # import cv2
 from glob import glob
 import hashlib
+import logging
 from PIL import Image
 img_size = 0
 totalImageSize = 0
@@ -30,28 +31,32 @@ def find_dir(dir_path):
                 # return full_paths
     print(full_paths)
     return full_paths
+
+
 def checkSameResource(dir_path,f):
     # 获取md5的资源
     md5Dict = get_all_md5(dir_path)
     for root , dirs , files in os.walk(dir_path):
         for file in files:
             fileFullPath = os.path.join(root,file)
+            # print(fileFullPath)
             md5 = get_md5(fileFullPath)
             same_files = md5Dict[md5]
-            if len(same_files) > 1:
-                if fileFullPath.endswith(".png") or fileFullPath.endswith(".jpg"):
+            # print(len(same_files))
+            if len(same_files) >= 1:
+                if fileFullPath.endswith(".png") or fileFullPath.endswith(".jpg") or fileFullPath.endswith("..JPG"):
                     # 写进图片信息中去
-                    writeImageInfo(f,fileFullPath[len(dir_path)+1:],same_files)
+                    writeImageInfo(f,fileFullPath,same_files)
 
-
-def writeImageInfo(f,image_full_path,files):
-    img = Image.open(image_full_path)
-    totalImageSize = os.path.getsize(image_full_path)
-    f.write(image_full_path + ',')
+def writeImageInfo(f,file_full_path,files):
+    global totalImageSize
+    img = Image.open(file_full_path)
+    totalImageSize = os.path.getsize(file_full_path)
+    f.write(file_full_path + ',')
     f.write(img.format + ',')
     f.write(str(img.size[0]) + ',')
     f.write(str(img.size[1]) + ',')
-    f.write(str(os.path.getsize(image_full_path)/1024) + ',')
+    f.write(str(os.path.getsize(file_full_path)/1024) + ',')
     f.write(str(len(files))+ ',')
     for file in files:
         f.write(str(file) + ',')
@@ -65,6 +70,8 @@ def get_md5(file_full_path):
     :param dir_path:
     :return:
     '''
+    if not os.path.isfile(file_full_path):
+        return
     myhash = hashlib.md5()
     f = open(file_full_path,'rb')
     while True:
@@ -86,40 +93,19 @@ def get_all_md5(dir_path):
                 md5Drict[md5] = filelist
             else:
                 md5Drict[md5].append(file_full_path[len(dir_path)+1:])
+    print("md5Drict"+str(md5Drict))
     return md5Drict
-#
-# def get_all_image(dir_path):
-#     '''
-#     获取图片和大小
-#     :param filename:
-#     :return:
-#     '''
-#     # find_dir(dir_path)
-#     img_list = []
-#     global img_size
-#     for img_path in find_dir(dir_path):
-#         img = os.path.splitext(img_path)
-#         im = Image.open(img_path)
-#         im_size =im.size
-#         img_size = os.path.getsize(img_path)
-#         img_list.append(im)
-#         # for img_cp in img_list:
-#         #     img_cp
-#         # print("图片名称："+img_path+"图片尺寸:"+str(im_size)+"--图片大小:"+str(img_size/1024))
-#     print("图片:%s"+str(img_list))
-
-
 
 if __name__ == "__main__":
-    # get_all_image("C:\\Users\wb.liuxiandong\Documents\GitHub\Casino_Client\Assets")
-    dir_path = 'C:\\Users\wb.liuxiandong\Documents\GitHub\Casino_Client\Assets'
+    # get_all_image("C:\\Users\\admin\\Pictures")
+    dir_path = 'C:\\Users\\admin\Pictures'
     if os.path.exists(OUTPUT_PATH):
         os.remove(OUTPUT_PATH)
-    f = open(OUTPUT_PATH,"w+")
+    f = open(OUTPUT_PATH,"w")
     f.write("path,format,width,height,size,count,file1,file2,file3,file4,file5\n")
-    for file in dir_path:
-        checkSameResource(file,f)
-    print(totalImageSize)
+    # for file in dir_path:
+    checkSameResource(dir_path,f)
+    print("totalImageSize:"+ str(totalImageSize))
     f.write('totalImageSize' + ',' + str(totalImageSize/1024) + 'K\n')
     f.close()
     # for file in dir_path:
